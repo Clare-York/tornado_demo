b6a2c4cf3511fa75afd807d91836ce4badde7281
cy <cy@shv.im>
2021-01-04 14:53:29 +0800

    修改路由/test的处理逻辑

--- handlers/TestHandlers.py
+++ handlers/TestHandlers.py
@@ -21,23 +21,67 @@ class TestHandler(tornado.web.RequestHandler, ABC):
         处理get请求
         :return:
         """
-        self.write("Headers: %s" % self.request.headers)
-        self.write("Method: %s" % self.request.method)
-        self.write("Query: %s" % self.request.query)
-        self.write("Query_Arguments: %s" % self.request.query_arguments)
-        self.write("Body: %s" % self.request.body)
-        self.write("Body_Arguments: %s" % self.request.body_arguments)
-        self.write("Arguments: %s" % self.request.arguments)
-        self.write("Files: %s" % self.request.files)
-        self.write("Remote_IP: %s" % self.request.remote_ip)
-        self.write("Host: %s" % self.request.host)
-        self.write("Host_Name: %s" % self.request.host_name)
-        self.write("Uri: %s" % self.request.uri)
-        self.write("Path: %s" % self.request.path)
-        self.write("Connection: %s" % str(self.request.connection))
-        self.write("Server_Connection: %s" % str(self.request.server_connection))
-        self.write("Version: %s" % self.request.version)
-        self.write("Protocol: %s" % self.request.protocol)
+        self.write("Headers: %s" % self.request.headers)  # 请求头
+
+        self.write("<p></p>")
+
+        self.write("Method: %s" % self.request.method)  # 请求方式
+
+        self.write("<p></p>")
+
+        self.write("Query: %s" % self.request.query)  # get请求?后拼接的内容
+
+        self.write("<p></p>")
+
+        self.write("Query_Arguments: %s" % self.request.query_arguments)  # get请求的参数
+
+        self.write("<p></p>")
+
+        self.write("Body: %s" % self.request.body)  # 请求体
+
+        self.write("<p></p>")
+
+        self.write("Body_Arguments: %s" % self.request.body_arguments)  # 请求体内的参数
+
+        self.write("<p></p>")
+
+        self.write("Arguments: %s" % self.request.arguments)  # 请求参数
+
+        self.write("<p></p>")
+
+        self.write("Files: %s" % self.request.files)  # 上传的文件
+
+        self.write("<p></p>")
+
+        self.write("Remote_IP: %s" % self.request.remote_ip)  # 来自IP
+
+        self.write("<p></p>")
+
+        self.write("Host: %s" % self.request.host)  # 服务运行地址
+
+        self.write("<p></p>")
+
+        self.write("Host_Name: %s" % self.request.host_name)  # 运行IP
+
+        self.write("<p></p>")
+
+        self.write("Uri: %s" % self.request.uri)  # 请求的路由+参数
+
+        self.write("<p></p>")
+
+        self.write("Path: %s" % self.request.path)  # 请求的具体路由
+
+        self.write("<p></p>")
+
+        self.write("Cookies: %s" % self.request.cookies)  # 未知
+
+        self.write("<p></p>")
+
+        self.write("Version: %s" % self.request.version)  # http协议版本
+
+        self.write("<p></p>")
+
+        self.write("Protocol: %s" % self.request.protocol)  # http/https协议
 
     def post(self):
         """
@@ -48,7 +92,8 @@ class TestHandler(tornado.web.RequestHandler, ABC):
         try:
             parameter_a = self.get_argument("a")
         except tornado.web.MissingArgumentError:
-            raise tornado.web.HTTPError(400)
+            # raise tornado.web.HTTPError(400)
+            print("获取参数'a'失败！")
         else:
             print("a:", parameter_a)
 
@@ -58,48 +103,64 @@ class TestHandler(tornado.web.RequestHandler, ABC):
             filename = file_data["filename"]  # 文件名
             filebody = file_data["body"]  # 文件主体
             fileext = file_data["content_type"]  # 文件类型
-        except Exception:
-            raise tornado.web.HTTPError(400)
+        except KeyError:
+            # raise tornado.web.HTTPError(401)
+            print('获取参数失败')
+        except Exception as e:
+            # raise tornado.web.HTTPError(400)
+            print(e)
         else:
             if fileext.split('/')[0] == "image":  # 以图片为例
-                file = open(filename, "wb")
-                file.write(filebody)
-                file.close()
+                f = open(filename, "wb")
+                f.write(filebody)
+                f.close()
 
         # 获取上传的文件-多文件
         try:
-            file_data = self.request.files["file"][0]
-            for file in file_data:
-                filename = file["filename"]
-                filebody = file["body"]
-                fileext = file["content_type"]
-        except Exception:
-            raise tornado.web.HTTPError(400)
+            fileExt = None
+            fileName = None
+            fileBody = None
+            file_Data = self.request.files["file"][0]
+            for file in file_Data:
+                fileName = file["filename"]
+                fileBody = file["body"]
+                fileExt = file["content_type"]
+        except KeyError:
+            # raise tornado.web.HTTPError(401)
+            print('获取参数失败')
+        except Exception as e:
+            print(e)
+            # raise tornado.web.HTTPError(400)
         else:
-            if fileext.split('/')[0] == "image":  # 以图片为例
-                file = open(filename, 'wb')
-                file.write(filebody)
+            if fileExt.split('/')[0] == "image":  # 以图片为例
+                file = open(fileName, 'wb')
+                file.write(fileBody)
                 file.close()
 
         # 获取上传的图片-base64
         try:
-            img_data = self.get_argument("image")
-            image_data = base64.b64decode(img_data)
-        except Exception:
-            raise tornado.web.HTTPError(400)
+            image_data = base64.b64decode(self.get_argument("image"))
+        except KeyError:
+            # raise tornado.web.HTTPError(401)
+            print('获取参数失败')
+        except Exception as e:
+            print(e)
+            # raise tornado.web.HTTPError(400)
         else:
-            f = open("test.jpg", 'wb')
-            f.write(image_data)
-            f.close()
+            img = open("test.jpg", 'wb')
+            img.write(image_data)
+            img.close()
 
         # 获取json数据
         try:
             data = json.loads(self.request.body)
             parameter_a = data["a"]
         except json.decoder.JSONDecodeError:  # 不是json数据
-            raise tornado.web.HTTPError(403)
+            print("不是Json数据")
+            # raise tornado.web.HTTPError(403)
         except KeyError:  # 没有找到key
-            raise tornado.web.HTTPError(405)
+            print('获取参数失败')
+            # raise tornado.web.HTTPError(405)
         else:
             print("a:", parameter_a)
 
