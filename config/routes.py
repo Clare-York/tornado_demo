39f84a80c7da4fa7af7758c905a878efa98e64e1
cy <cy@shv.im>
2021-01-04 14:50:21 +0800

    自定义404路由

--- config/routes.py
+++ config/routes.py
@@ -7,10 +7,12 @@
 """
 from handlers.MainHandlers import MainHandler
 from handlers.TestHandlers import TestHandler
+from handlers.PageNotFoundHandlers import PageNotFoundHandler
 
 # 将路由存入到列表中，后续注册对应视图到server端，告诉server端不同地址，
 urls = [
     (r"/", MainHandler),  # 根路由
     (r"/test", TestHandler),  # 测试
 
+    (r".*", PageNotFoundHandler),  # 自定义404页，这一条放在所有路由的最后，否则下方的路由均被导向到404页
 ]
