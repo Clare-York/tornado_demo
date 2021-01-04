4ddaae0fc07eb0f2e14819738137595f6a34abc3
cy <cy@shv.im>
2021-01-04 13:33:35 +0800

    add settings_template.py

---
+++ config/settings_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@Time        : 2021/1/4 13:00
+@Author      : chengyao
+@File        : settings_template.py
+@introduce   :
+"""
+DEBUG = False
+
+HOST = "127.0.0.1"
+
+PORT = 8080
