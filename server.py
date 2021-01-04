ed23560fe0ce45f22defbfba753b67826661b67e
cy <cy@shv.im>
2021-01-04 15:31:33 +0800

    修改部分注释

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 @Time        : 2021/1/4 9:50
 @Author      : chengyao
 @File        : server.py
-@introduce   :
+@introduce   : tornado服务主程序
 """
 import tornado.ioloop
 import tornado.httpserver
@@ -12,24 +12,23 @@ from Core.applications import application
 import sys
 from Config.settings import HOST, PORT
 
-# 定义一个变量，options.port = 8080。这样的好处是，
-# 如果之后需要在tornado其他地方使用这个变量，
-# 就可以直接用options.port就可以获得
+# 定义一个变量，options.port = 8080。
+# 这样的好处是，如果之后需要在其他地方使用这个变量，就可以直接用options.port就可以获得
+
 tornado.options.define("port", default=PORT, help="run on this port", type=int)
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
-    # 根据application的设置，生成一个server
-    http_server = tornado.httpserver.HTTPServer(application)
-    # server调用监听方法，监听options.port端口
-    # http_server.listen(tornado.options.options.port, address=HOST)
+    tornado.options.parse_command_line()  # 加入此行可在运行时利用命令行--port xxx 指定运行端口
+
+    http_server = tornado.httpserver.HTTPServer(application)  # 根据application的设置，生成一个http_server
+
+    # http_server.listen(tornado.options.options.port, address=HOST)  # 运行在address上，监听options.port端口
 
     http_server.bind(tornado.options.options.port, address=HOST)  # 多进程启动http_server，windows下不可用
-    # 指定开启几个进程，默认值为1；
-    # 如果参数num_processes为None或<=0，则自动根据机器硬件的cpu核芯数创建同等数目的子进程；如果参数值>0，则创建num_processes个子进程。
-    http_server.start()
+
+    http_server.start()  # 默认num_processes=1，当值<=0或None则自动根据cpu核芯数创建同等数目的子进程,>0则创建指定的子进程
 
     lib = sys.path[0] + '\t' + HOST + '\t' + str(tornado.options.options.port)  # 为了打印现在server是来自哪个文件，以及在监听哪个端口
     print('Tornado server is running at %s' % lib)
-    # 启动tornado服务
-    tornado.ioloop.IOLoop.instance().start()
+
+    tornado.ioloop.IOLoop.instance().start()  # 启动tornado服务
